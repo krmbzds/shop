2e080d29c44758a69bdb0910925828b55447b988
Kerem Bozdas <krmbzds.github@gmail.com>
2016-06-13 14:18:22 +0300

    Reorganize classes and add associations

--- app.rb
+++ app.rb
@@ -13,22 +13,24 @@ use Rack::Session::Cookie, :secret => "TODO: CHANGE ME"
 use Rack::Csrf, :raise => true
 
 class User < ActiveRecord::Base
+  has_many :carts
   validates_presence_of :name, :email, :password
+end
 
+class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :cart_items
 end
 
 class Product < ActiveRecord::Base
   validates_presence_of :name, :price
-
 end
-
-class Cart < ActiveRecord::Base; end
 
 class CartItem < ActiveRecord::Base
+  belongs_to :cart
+  has_one :product
   validates_presence_of :quantity
-  
 end
-
 
 get "/" do
   erb :index
