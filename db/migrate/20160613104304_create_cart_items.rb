2767b0ce22eabefbfc1a3490ecf4f102fe6a284e
Kerem Bozdas <krmbzds.github@gmail.com>
2016-06-13 15:15:56 +0300

    Add quantity to CartItems

--- db/migrate/20160613104304_create_cart_items.rb
+++ db/migrate/20160613104304_create_cart_items.rb
@@ -1,6 +1,7 @@
 class CreateCartItems < ActiveRecord::Migration
   def change
     create_table :cart_items do |t|
+      t.integer :quantity
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20160613113452) do
 
   create_table "cart_items", force: :cascade do |t|
+    t.integer  "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "cart_id"
